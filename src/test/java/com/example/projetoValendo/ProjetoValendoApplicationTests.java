ac0eca1f5af05b8e4d6ca5d2d80b4049ada76baa
Vitor Nagaya Wong <vnwgithub@gmail.com>
2025-02-18 21:12:31 -0300

    first commit

---
+++ src/test/java/com/example/projetoValendo/ProjetoValendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoValendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoValendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
